cca53d8490ad80e57df3807d7a273fd6d8b7f0c6
J Stutterheim <jorstut@hotmail.com>
2023-03-08 14:51:07 +0100

    test pipleine

--- infrastructure/dev/function.tf
+++ infrastructure/dev/function.tf
@@ -1,15 +1,15 @@
-module "user_management" {
-  source  = "../modules/function_app"
+# module "user_management" {
+#   source  = "../modules/function_app"
 
-  name                = "func-${var.workload_name}-userManagement-${var.env}"
-  product_name        = "userManagement"
-  resource_group_name = azurerm_resource_group.dev.name
-  location            = azurerm_resource_group.dev.location
-  tags = local.tags
-  storage_account_name       = azurerm_storage_account.func_storage.name
-  storage_account_access_key = azurerm_storage_account.func_storage.primary_access_key
-  application_insights_connection_string = azurerm_application_insights.appi.connection_string
-}
+#   name                = "func-${var.workload_name}-userManagement-${var.env}"
+#   product_name        = "userManagement"
+#   resource_group_name = azurerm_resource_group.dev.name
+#   location            = azurerm_resource_group.dev.location
+#   tags = local.tags
+#   storage_account_name       = azurerm_storage_account.func_storage.name
+#   storage_account_access_key = azurerm_storage_account.func_storage.primary_access_key
+#   application_insights_connection_string = azurerm_application_insights.appi.connection_string
+# }
 
 # module "session_management" {
 #   source  = "../modules/function_app"
