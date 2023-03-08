ec03fe802eeedc2597140bd48199cc5067d9175d
J Stutterheim <jorstut@hotmail.com>
2023-03-08 11:48:58 +0100

    added all prelimanary functions and databases

--- infrastructure/shared/dev/databases.tf
+++ infrastructure/shared/dev/databases.tf
@@ -5,3 +5,19 @@ module "userdb" {
   server_id      = azurerm_mssql_server.sqlserver.id
   tags = local.tags
 }
+
+module "userdb" {
+  source  = "../../modules/database"
+
+  name           = "sqldb-game-${var.env}"
+  server_id      = azurerm_mssql_server.sqlserver.id
+  tags = local.tags
+}
+
+module "userdb" {
+  source  = "../../modules/database"
+
+  name           = "sqldb-session-${var.env}"
+  server_id      = azurerm_mssql_server.sqlserver.id
+  tags = local.tags
+}
