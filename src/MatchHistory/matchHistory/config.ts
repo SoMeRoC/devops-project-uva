f324461698a788a4ff5a599b36dd61efcd7ffa19
Quinten Coltof <quintencoltof1@gmail.com>
2023-03-09 13:41:10 +0000

    Feat: Get match history Feat: Sample create match history

---
+++ src/MatchHistory/matchHistory/config.ts
@@ -0,0 +1,19 @@
+import { Dialect } from "sequelize"
+
+type SQLConfig = {
+  hostName: string
+  userName: string
+  password: string
+  database: string
+  dialect: Dialect
+  dialectOptions: Object
+}
+
+export const sql: SQLConfig = {
+  hostName: process.env.MYSQL_HOST || 'db',
+  userName: process.env.MYSQL_USER || 'guest',
+  password: process.env.MYSQL_PASSWORD || 'guest',
+  database: process.env.MYSQL_DATABASE || 'matchHistory',
+  dialect: process.env.MYSQL_DIALECT as Dialect || 'mysql', // mssql for azure
+  dialectOptions: {}, // { encrypt: true } for azure
+}
