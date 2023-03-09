ce1866fa2e0050e07cf66836fc64bb955999aa52
J Stutterheim <jorstut@hotmail.com>
2023-03-09 16:31:25 +0100

    template pipeline

--- infrastructure/dev/function.tf
+++ infrastructure/dev/function.tf
@@ -24,29 +24,29 @@ module "session_management" {
   application_insights_connection_string = azurerm_application_insights.appi.connection_string
 }
 
-# module "match_making" {
-#   source  = "../modules/function_app"
+module "match_making" {
+  source  = "../modules/function_app"
 
-#   name                = "func-${var.workload_name}-matchMaking-${var.env}"
-#   product_name        = "matchMaking"
-#   resource_group_name = azurerm_resource_group.dev.name
-#   location            = azurerm_resource_group.dev.location
-#   tags = local.tags
-#   storage_account_name       = azurerm_storage_account.func_storage.name
-#   storage_account_access_key = azurerm_storage_account.func_storage.primary_access_key
-#   application_insights_connection_string = azurerm_application_insights.appi.connection_string
-# }
+  name                = "func-${var.workload_name}-matchMaking-${var.env}"
+  product_name        = "matchMaking"
+  resource_group_name = azurerm_resource_group.dev.name
+  location            = azurerm_resource_group.dev.location
+  tags = local.tags
+  storage_account_name       = azurerm_storage_account.func_storage.name
+  storage_account_access_key = azurerm_storage_account.func_storage.primary_access_key
+  application_insights_connection_string = azurerm_application_insights.appi.connection_string
+}
 
 
-# module "game" {
-#   source  = "../modules/function_app"
+module "game" {
+  source  = "../modules/function_app"
 
-#   name                = "func-${var.workload_name}-game-${var.env}"
-#   product_name        = "game"
-#   resource_group_name = azurerm_resource_group.dev.name
-#   location            = azurerm_resource_group.dev.location
-#   tags = local.tags
-#   storage_account_name       = azurerm_storage_account.func_storage.name
-#   storage_account_access_key = azurerm_storage_account.func_storage.primary_access_key
-#   application_insights_connection_string = azurerm_application_insights.appi.connection_string
-# }
+  name                = "func-${var.workload_name}-game-${var.env}"
+  product_name        = "game"
+  resource_group_name = azurerm_resource_group.dev.name
+  location            = azurerm_resource_group.dev.location
+  tags = local.tags
+  storage_account_name       = azurerm_storage_account.func_storage.name
+  storage_account_access_key = azurerm_storage_account.func_storage.primary_access_key
+  application_insights_connection_string = azurerm_application_insights.appi.connection_string
+}
