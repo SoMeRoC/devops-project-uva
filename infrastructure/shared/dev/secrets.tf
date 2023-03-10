a9856cac0ac0144b209eb6041d4082167af6099c
J Stutterheim <jorstut@hotmail.com>
2023-03-10 14:05:57 +0100

    All connectionStrings in KV

--- infrastructure/shared/dev/secrets.tf
+++ infrastructure/shared/dev/secrets.tf
@@ -17,3 +17,24 @@ resource "azurerm_key_vault_secret" "db_connection_string_game" {
   value        = format("Server=tcp:sql-someroc-shared-dev.database.windows.net,1433;Initial Catalog=sqldb-game-dev;Persist Security Info=False;User ID=someroc_admin;Password=%s;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;",random_password.db_root.result)
   key_vault_id = azurerm_key_vault.kv.id
 }
+
+resource "azurerm_key_vault_secret" "db_connection_string_user" {
+  depends_on = [
+    azurerm_role_assignment.spn_to_kv
+  ]
+
+  name         = "userConnection"
+  value        = format("Server=tcp:sql-someroc-shared-dev.database.windows.net,1433;Initial Catalog=sqldb-user-dev;Persist Security Info=False;User ID=someroc_admin;Password=%s;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;",random_password.db_root.result)
+  key_vault_id = azurerm_key_vault.kv.id
+}
+
+
+resource "azurerm_key_vault_secret" "db_connection_string_session" {
+  depends_on = [
+    azurerm_role_assignment.spn_to_kv
+  ]
+
+  name         = "sessionConnection"
+  value        = format("Server=tcp:sql-someroc-shared-dev.database.windows.net,1433;Initial Catalog=sqldb-session-dev;Persist Security Info=False;User ID=someroc_admin;Password=%s;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;",random_password.db_root.result)
+  key_vault_id = azurerm_key_vault.kv.id
+}
