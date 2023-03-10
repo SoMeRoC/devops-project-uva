a08ff16980e82fda4846569b28f9893fad6688ea
J Stutterheim <jorstut@hotmail.com>
2023-03-10 12:27:58 +0100

    group role assignment to KV

--- infrastructure/shared/dev/role_assignments.tf
+++ infrastructure/shared/dev/role_assignments.tf
@@ -3,3 +3,9 @@ resource "azurerm_role_assignment" "spn_to_kv" {
   role_definition_name             = "Key Vault Secrets Officer"
   principal_id                     = data.azurerm_client_config.current.object_id
 }
+
+resource "azurerm_role_assignment" "group_to_kv" {
+  scope                            = azurerm_key_vault.kv.id
+  role_definition_name             = "Key Vault Secrets Officer"
+  principal_id                     = data.azuread_group.group.object_id
+}
