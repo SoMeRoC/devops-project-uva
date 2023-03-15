89472dd4140f7d71c59361f5faf9a65301f40f67
J Stutterheim <jorstut@hotmail.com>
2023-03-15 12:17:41 +0100

    FE pipeline

--- infrastructure/dev/role_assignments.tf
+++ infrastructure/dev/role_assignments.tf
@@ -4,14 +4,14 @@ resource "azurerm_role_assignment" "func_users_to_kv" {
   principal_id                     = module.user_management.principal_id
 }
 
-resource "azurerm_role_assignment" "webapp_to_kv" {
-  scope                            = data.azurerm_key_vault.kv.id
-  role_definition_name             = "Key Vault Secrets Officer"
-  principal_id                     = azurerm_linux_web_app.fe.identity[0].principal_id
-}
+# resource "azurerm_role_assignment" "webapp_to_kv" {
+#   scope                            = data.azurerm_key_vault.kv.id
+#   role_definition_name             = "Key Vault Secrets Officer"
+#   principal_id                     = azurerm_linux_web_app.fe.identity[0].principal_id
+# }
 
-resource "azurerm_role_assignment" "webapp_cert_to_kv" {
-  scope                            = data.azurerm_key_vault.kv.id
-  role_definition_name             = "Key Vault Certificates Officer"
-  principal_id                     = azurerm_linux_web_app.fe.identity[0].principal_id
-}
+# resource "azurerm_role_assignment" "webapp_cert_to_kv" {
+#   scope                            = data.azurerm_key_vault.kv.id
+#   role_definition_name             = "Key Vault Certificates Officer"
+#   principal_id                     = azurerm_linux_web_app.fe.identity[0].principal_id
+# }
