57c7624cf556bd771ffd8de79348ef50111c7b11
J Stutterheim <jorstut@hotmail.com>
2023-03-15 12:20:02 +0100

    FE pipeline

--- infrastructure/dev/webapp.tf
+++ infrastructure/dev/webapp.tf
@@ -1,35 +1,35 @@
 
-# resource "azurerm_service_plan" "app" {
-#   name                = "apps-FrontEnd-${var.env}"
-#   resource_group_name = azurerm_resource_group.frontend.name
-#   location            = azurerm_resource_group.frontend.location
-#   os_type             = "Linux"
-#   sku_name            = "B1"
-# }
+resource "azurerm_service_plan" "app" {
+  name                = "apps-FrontEnd-${var.env}"
+  resource_group_name = azurerm_resource_group.frontend.name
+  location            = azurerm_resource_group.frontend.location
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
 
-# resource "azurerm_linux_web_app" "fe" {
-#   name                = "web-${var.workload_name}-FrontEnd-${var.env}"
-#   resource_group_name = azurerm_resource_group.frontend.name
-#   location            = azurerm_service_plan.app.location
-#   service_plan_id     = azurerm_service_plan.app.id
+resource "azurerm_linux_web_app" "fe" {
+  name                = "web-${var.workload_name}-FrontEnd-${var.env}"
+  resource_group_name = azurerm_resource_group.frontend.name
+  location            = azurerm_service_plan.app.location
+  service_plan_id     = azurerm_service_plan.app.id
 
-#   app_settings = {
-#     "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.appi.connection_string
-#   }
+  app_settings = {
+    "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.appi.connection_string
+  }
 
-#   site_config {
-#     application_stack {
-#         node_version = "18-lts"
-#     }
-#   }
+  site_config {
+    application_stack {
+        node_version = "18-lts"
+    }
+  }
 
-#   identity {
-#     type = "SystemAssigned"
-#   }
+  identity {
+    type = "SystemAssigned"
+  }
 
-#   lifecycle {
-#     ignore_changes = [
-#       tags,
-#     ]
-#   }
-# }
+  lifecycle {
+    ignore_changes = [
+      tags,
+    ]
+  }
+}
