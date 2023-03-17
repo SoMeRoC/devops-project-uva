bed86a8dfa1b504da0b47af33f1116541c3f4d8a
J Stutterheim <jorstut@hotmail.com>
2023-03-17 16:49:48 +0100

    try again

--- src/Game/writeToTableExample/index.ts
+++ src/Game/writeToTableExample/index.ts
@@ -3,13 +3,14 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
     const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
+    var responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
     context.bindings.tableBinding = [];
 
     for (var i = 1; i < 10; i++) {
+        responseMessage = responseMessage + i.toString()
         context.bindings.tableBinding.push({
             PartitionKey: "Test",
             RowKey: i.toString(),
