2504c78ec981ef1dd695d177bb9172c3ada09e6d
Jorrit05 <jorstut@hotmail.com>
2023-03-18 22:07:55 +0100

    Update userDb-table-scheme.sql

--- infrastructure/sql/userDb-table-scheme.sql
+++ infrastructure/sql/userDb-table-scheme.sql
@@ -7,7 +7,6 @@ CREATE TABLE users (
   first_name VARCHAR(50) NOT NULL,
   last_name VARCHAR(50) NOT NULL,
   email VARCHAR(100) NOT NULL UNIQUE,
-  password VARCHAR(100) NOT NULL,
   admin BIT NOT NULL DEFAULT 0
 );
 
