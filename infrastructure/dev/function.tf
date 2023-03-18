9536520568310a13b7caaa45056ca39794853391
J Stutterheim <jorstut@hotmail.com>
2023-03-18 15:20:10 +0100

    re-add game

--- infrastructure/dev/function.tf
+++ infrastructure/dev/function.tf
@@ -41,16 +41,16 @@ module "match_making" {
 }
 
 
-# module "game" {
-#   source  = "../modules/function_app"
+module "game" {
+  source  = "../modules/function_app"
 
-#   name                = "func-${var.workload_name}-game-${var.env}"
-#   product_name        = "game"
-#   resource_group_name = azurerm_resource_group.dev.name
-#   location            = azurerm_resource_group.dev.location
-#   tags = local.tags
-#   storage_account_name       = azurerm_storage_account.func_storage.name
-#   storage_account_access_key = azurerm_storage_account.func_storage.primary_access_key
-#   application_insights_connection_string = azurerm_application_insights.appi.connection_string
-#   app_conf_connection_string = data.azurerm_key_vault_secret.appconf.value
-# }
+  name                = "func-${var.workload_name}-gameEngine-${var.env}"
+  product_name        = "game"
+  resource_group_name = azurerm_resource_group.dev.name
+  location            = azurerm_resource_group.dev.location
+  tags = local.tags
+  storage_account_name       = azurerm_storage_account.func_storage.name
+  storage_account_access_key = azurerm_storage_account.func_storage.primary_access_key
+  application_insights_connection_string = azurerm_application_insights.appi.connection_string
+  app_conf_connection_string = data.azurerm_key_vault_secret.appconf.value
+}
