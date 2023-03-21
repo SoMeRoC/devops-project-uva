92f7c8ecd6368a2e831e415650495e16a87391a8
Quinten Coltof <quintencoltof1@gmail.com>
2023-03-21 15:02:38 +0000

    Feat: Send disconnection event

--- src/SessionManager/disconnected/index.ts
+++ src/SessionManager/disconnected/index.ts
@@ -1,18 +1,34 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { Op } from "sequelize";
+import { Session } from "../db";
 
-const Diconnect: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-  return;
+const DisconnectUser: AzureFunction = async function (context: Context, req: HttpRequest, wpsReq): Promise<Object> {
+  const { connectionId } = wpsReq.request.connectionContext;
 
-  const { userId, sessionId } = context.bindingData.connectionContext;
+  const session = await Session.findOne({
+    where:
+    {
+      [Op.or]: [
+        { blackConId: connectionId },
+        { whiteConId: connectionId }
+      ]
+    }
+  });
+  if (!session) {
+    context.res = { status: 404, body: 'Session not found.' }
+    return;
+  }
+
+  const sessionId = session.dataValues.id;
+  const color = session.dataValues.whiteConId === connectionId ? 'w' : 'b';
 
   context.bindings.actions = [
     {
-      actionName: 'RemoveUserFromGroup',
-      userId: userId,
+      actionName: 'sendToGroup',
       group: `session-${sessionId}`,
-    }
+      data: JSON.stringify({ event: 'playerDisconnected', color: color }),
+    },
   ];
-  return;
 };
 
-export default Diconnect;
+export default DisconnectUser;
