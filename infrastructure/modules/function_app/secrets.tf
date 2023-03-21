5df29cf01dcbfd7e9b5312f22ac7cb78ad4fe962
J Stutterheim <jorstut@hotmail.com>
2023-03-21 21:17:37 +0100

    Keys

--- infrastructure/modules/function_app/secrets.tf
+++ infrastructure/modules/function_app/secrets.tf
@@ -1,4 +1,13 @@
 
+data "azurerm_key_vault" "kv" {
+    name = "kv-${var.workload_name}-shared-${var.env}"
+    resource_group_name = data.azurerm_resource_group.shared.name
+}
+
+data "azurerm_resource_group" "shared" {
+    name = "rg-${var.workload_name}-shared-${var.env}"
+}
+
 data "azurerm_key_vault_secret" "gameApi" {
   name         = "gameApi"
   key_vault_id = data.azurerm_key_vault.kv.id
