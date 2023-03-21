fb8c9c89c183ef978617a0a2406b770b84597895
Quinten Coltof <quintencoltof1@gmail.com>
2023-03-21 18:31:32 +0000

    test 4

--- src/SessionManager/create/index.ts
+++ src/SessionManager/create/index.ts
@@ -2,8 +2,6 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 import { Session } from "../db";
 
 const CreateSessions: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-  const connectionString = (process.env.SqlConnectionString as string);
-  context.log(connectionString);
   context.log("1");
   const { players } = req.body;
   context.log(players);
@@ -18,20 +16,15 @@ const CreateSessions: AzureFunction = async function (context: Context, req: Htt
     return;
   }
 
+  const session = await Session.create({
+    start: new Date(),
+    white: players[0],
+    black: players[1],
+  })
+
   context.res = {
-    status: 200,
-    body: `Got it: ${connectionString}`
+    body: session.dataValues.id,
   }
-
-  // const session = await Session.create({
-  //   start: new Date(),
-  //   white: players[0],
-  //   black: players[1],
-  // })
-
-  // context.res = {
-  //   body: session.dataValues.id,
-  // }
   return;
 };
 
