2d001ef3fa364143c59446871b192c8302dcb4b2
Quinten Coltof <quintencoltof1@gmail.com>
2023-03-21 20:46:34 +0000

    oke nu dan?

--- src/SessionManager/connected/index.ts
+++ src/SessionManager/connected/index.ts
@@ -3,43 +3,42 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 // import { Session } from "../db";
 
 const ConnectUser: AzureFunction = async function (context: Context, req: HttpRequest, wpsReq): Promise<Object> {
-  return;
-  const { connectionId } = wpsReq.request.connectionContext;
+  // const { connectionId } = wpsReq.request.connectionContext;
 
-  const session = await Session.findOne({
-    where:
-    {
-      [Op.or]: [
-        { blackConId: connectionId },
-        { whiteConId: connectionId }
-      ]
-    }
-  });
-  if (!session) {
-    context.res = { status: 404, body: 'Session not found.' }
-    return;
-  }
+  // const session = await Session.findOne({
+  //   where:
+  //   {
+  //     [Op.or]: [
+  //       { blackConId: connectionId },
+  //       { whiteConId: connectionId }
+  //     ]
+  //   }
+  // });
+  // if (!session) {
+  //   context.res = { status: 404, body: 'Session not found.' }
+  //   return;
+  // }
 
-  const sessionId = session.dataValues.id;
-  const color = session.dataValues.whiteConId === connectionId ? 'w' : 'b';
+  // const sessionId = session.dataValues.id;
+  // const color = session.dataValues.whiteConId === connectionId ? 'w' : 'b';
 
-  context.bindings.actions = [
-    {
-      actionName: 'sendToConnection',
-      connectionId,
-      data: JSON.stringify({ event: 'handshake', color })
-    },
-    {
-      actionName: 'AddConnectionToGroup',
-      connectionId: connectionId,
-      group: `session-${sessionId}`,
-    },
-    {
-    actionName: 'sendToGroup',
-    group: `session-${sessionId}`,
-    data: JSON.stringify({ event: 'playerConnected', color: color }),
-  },
-  ];
+  // context.bindings.actions = [
+  //   {
+  //     actionName: 'sendToConnection',
+  //     connectionId,
+  //     data: JSON.stringify({ event: 'handshake', color })
+  //   },
+  //   {
+  //     actionName: 'AddConnectionToGroup',
+  //     connectionId: connectionId,
+  //     group: `session-${sessionId}`,
+  //   },
+  //   {
+  //   actionName: 'sendToGroup',
+  //   group: `session-${sessionId}`,
+  //   data: JSON.stringify({ event: 'playerConnected', color: color }),
+  // },
+  // ];
 };
 
 export default ConnectUser;
