e8072323335bb7a356d8790c9095b7f65ed0e841
Quinten Coltof <quintencoltof1@gmail.com>
2023-03-21 20:37:50 +0000

    Fix: Remove sequalize from config

--- src/SessionManager/config.ts
+++ src/SessionManager/config.ts
@@ -1,21 +1,3 @@
-import { Dialect } from "sequelize"
-
-type SQLConfig = {
-  hostName: string
-  userName: string
-  password: string
-  database: string
-  dialect: Dialect
-}
-
-export const sql: SQLConfig = {
-  hostName: process.env.MYSQL_HOST || 'db',
-  userName: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'root',
-  database: process.env.MYSQL_DATABASE || 'sessionManager',
-  dialect: process.env.MYSQL_DIALECT as Dialect || 'mysql', // mssql for azure
-}
-
 export const gameService = {
   url: process.env.GAME_URL || 'http://localhost:3003/',
   apiToken: process.env.API_TOKEN || '',
